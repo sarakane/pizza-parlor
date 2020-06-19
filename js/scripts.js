7bac8a1de71d70cc5030842b7454aeeb372f7cb1
Sara Kane <kane.sarak@gmail.com>
2020-06-19 08:47:40 -0700

    add prototype method to calculate the cost of the pizza

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,16 @@ function Pizza (toppings, size) {
   this.toppings = toppings;
   this.size = size;
 }
+
+Pizza.prototype.price = function() {
+  const numberOfToppings = this.toppings.length;
+  let price = numberOfToppings * 0.5;
+  if (size === "small") {
+    price = price + 14.15 ;
+  } else if (size === "medium") {
+    price = price + 16.75;
+  } else if (size === "large") {
+    price = price + 19.45;
+  } 
+  return price;
+}
