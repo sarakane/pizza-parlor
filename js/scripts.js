1583ac1fb8332cbb624a4a667a96a839d4fb2470
Sara Kane <kane.sarak@gmail.com>
2020-06-19 10:01:43 -0700

    add form with radio inputs to select pizza size and a submit button

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Business Logic
 function Pizza (toppings, size) {
   this.toppings = toppings;
   this.size = size;
@@ -6,12 +7,18 @@ function Pizza (toppings, size) {
 Pizza.prototype.price = function() {
   const numberOfToppings = this.toppings.length;
   let price = numberOfToppings * 0.5;
-  if (size === "small") {
+  if (this.size === "small") {
     price = price + 14.15 ;
-  } else if (size === "medium") {
+  } else if (this.size === "medium") {
     price = price + 16.75;
-  } else if (size === "large") {
+  } else if (this.size === "large") {
     price = price + 19.45;
   } 
   return price;
 }
+
+
+//UI Logic
+$(document).ready(function(){
+
+});
